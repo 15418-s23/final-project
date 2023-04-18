c11cd347d8c57887137a71a41951d65f8a69464a
Yufei Shi <yfshi@cmu.edu>
2023-04-18 15:26:24 -0400

    [sequential, parallel] Update function signature

--- src/mcd-parallel.h
+++ src/mcd-parallel.h
@@ -1,6 +1,15 @@
 #include "lib/vec3.h"
 #include "lib/obj.h"
 
+/**
+ * mcd - Mesh Collision Detection
+ * 
+ * @param model_vertices - a vector of vectors of vertices, where each vector of
+ *                         corresponds to a model
+ * @param model_faces - a vector of vectors of faces, where each vector of faces
+ *                      corresponds to a model
+ * @return the minimum distance between any two faces in the models
+*/
 double mcd(std::vector< std::vector<Vec3> > model_vertices,
            std::vector< std::vector<Face> > model_faces) {
     return 1.0;
