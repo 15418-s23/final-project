148ff3903e5800d98180a3588adcb6b682741b14
Yufei Shi <yfshi@cmu.edu>
2023-04-18 14:52:17 -0400

    [main] Add model loading

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,12 @@ int main(int argc, char *argv[]) {
     std::string line;
     int line_number = 0;
     while (std::getline(input_file, line)) {
+        // skip comments
+        if (line.substr(0, 2) == "//") {
+            continue;
+        }
+
+        // load the model file paths and base coordinates
         if (line_number % 2 == 0) {
             model_file_paths.push_back(line);
         } else {
@@ -41,7 +47,27 @@ int main(int argc, char *argv[]) {
             iss >> x >> y >> z;
             base_coordinates.push_back(Vec3(x, y, z));
         }
+        
         line_number++;
+    }
+
+    /* Load the models */
+    std::vector< std::vector<Vec3> > model_vertices;
+    std::vector< std::vector<Face> > model_faces;
+
+    for (size_t i = 0; i < model_file_paths.size(); i++) {
+        std::string model_file_path = model_file_paths[i];
+        Vec3 base_coordinate = base_coordinates[i];
+
+        auto [vertices, faces] = load_obj(model_file_path);
+
+        // add the base coordinate to the vertices
+        for (size_t j = 0; j < vertices.size(); j++) {
+            vertices[j] = vertices[j] + base_coordinate;
+        }
+
+        model_vertices.push_back(vertices);
+        model_faces.push_back(faces);
     }
 
 
