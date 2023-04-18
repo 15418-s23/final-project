55ba0e6d7d6c3b0fd66345d5c065428c4064cb53
Yufei Shi <yfshi@cmu.edu>
2023-04-18 15:08:31 -0400

    [sequential, parallel] Add dummy functions

---
+++ src/mcd-parallel.h
@@ -0,0 +1,7 @@
+#include "lib/vec3.h"
+#include "lib/obj.h"
+
+double mcd(std::vector< std::vector<Vec3> > model_vertices,
+           std::vector< std::vector<Face> > model_faces) {
+    return 0.0;
+}
