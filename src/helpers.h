62e6692b360d0b95bf768767ce2e810f9ab3c57f
Yufei Shi <yfshi@cmu.edu>
2023-04-19 04:21:33 -0400

    [sequantial, helper] Sequential implementation attempt, something is not correct

---
+++ src/helpers.h
@@ -0,0 +1,141 @@
+
+#pragma once
+
+#include <cmath>
+
+#include "lib/vec3.h"
+
+
+static inline bool line_face_intersect(const Vec3& v1, const Vec3& v2,
+                                       const Vec3& u1, const Vec3& u2, const Vec3& u3) {
+    // compute the edges of face u
+    Vec3 eu1 = u2 - u1;
+    Vec3 eu2 = u3 - u1;
+
+    // compute the edge from face v
+    Vec3 ev = v2 - v1;
+
+    // compute the determinant
+    Vec3 h = cross(eu2, ev);
+    double det = static_cast<double>(dot(eu1, h));
+
+    // if the determinant is zero, the faces are parallel
+    if (det <= std::numeric_limits<double>::epsilon()) {
+        return false;
+    }
+
+    double inv_det = 1.0 / det;
+    Vec3 s = v1 - u1;
+
+    // compute the first barycentric coordinate
+    double u = static_cast<double>(dot(s, h)) * inv_det;
+
+    // if the intersection is outside the triangle face, return false
+    if (u < 0.0 || u > 1.0) {
+        return false;
+    }
+
+    // compute the second barycentric coordinate
+    Vec3 q = cross(s, ev);
+    double v = static_cast<double>(dot(ev, q)) * inv_det;
+
+    // if the intersection is outside the triangle face, return false
+    if (v < 0.0 || u + v > 1.0) {
+        return false;
+    }
+
+    // compute the distance from the intersection point to the first vertex of face v
+    double t = static_cast<double>(dot(eu2, q)) * inv_det;
+
+    // if the distance is non-negative, the intersection point is on the edge
+    return t >= 0.0;
+}
+
+
+static inline double point_to_edge_distance(const Vec3& v, const Vec3& u1, const Vec3& u2) {
+    // compute the edge from face u
+    Vec3 eu = u2 - u1;
+
+    // compute the vector from the first vertex of face u to the point
+    Vec3 s = v - u1;
+
+    // compute the projection of the vector onto the edge
+    double t = dot(s, eu) / dot(eu, eu);
+
+    // if the projection is outside the edge, return the minimum distance to the vertices
+    if (t < 0.0) {
+        return (v - u1).norm();
+    } else if (t > 1.0) {
+        return (v - u2).norm();
+    }
+
+    // otherwise, return the distance to the projection
+    return (v - (u1 + t * eu)).norm();
+}
+
+
+static inline double point_to_face_distance(const Vec3& v, const Vec3& u1, const Vec3& u2, const Vec3& u3) {
+    // compute the edges of face u
+    Vec3 eu1 = u2 - u1;
+    Vec3 eu2 = u3 - u1;
+
+    // compute the normal of face u
+    Vec3 normal = cross(eu1, eu2).normalize();
+
+    // compute the distance from the point to the plane of face u
+    double distance = dot(v - u1, normal);
+
+    // if the distance is negative, the point is on the wrong side of the plane
+    if (distance < 0.0) {
+        return std::numeric_limits<double>::max();
+    }
+
+    // compute the projection of the point onto the plane of face u
+    Vec3 projection = v - distance * normal;
+
+    // if the projection is inside the triangle, return the distance
+    if (line_face_intersect(v, projection, u1, u2, u3)) {
+        return distance;
+    }
+
+    // otherwise, return the minimum distance to the edges of face u
+    double min_distance = std::numeric_limits<double>::max();
+    min_distance = std::min(min_distance, point_to_edge_distance(v, u1, u2));
+    min_distance = std::min(min_distance, point_to_edge_distance(v, u2, u3));
+    min_distance = std::min(min_distance, point_to_edge_distance(v, u3, u1));
+    return min_distance;
+}
+
+
+static inline double edge_to_face_distance(const Vec3& v1, const Vec3& v2,
+                                           const Vec3& u1, const Vec3& u2, const Vec3& u3) {
+    // compute the edges of face u
+    Vec3 eu1 = u2 - u1;
+    Vec3 eu2 = u3 - u1;
+
+    // compute the normal of face u
+    Vec3 normal = cross(eu1, eu2).normalize();
+
+    // compute the distance from the first vertex of the edge to the plane of face u
+    double distance = dot(v1 - u1, normal);
+
+    // if the distance is negative, the edge is on the wrong side of the plane
+    if (distance < 0.0) {
+        return std::numeric_limits<double>::max();
+    }
+
+    // compute the projection of the first vertex of the edge onto the plane of face u
+    Vec3 projection = v1 - distance * normal;
+
+    // if the projection is inside the triangle, return the distance
+    if (line_face_intersect(v1, projection, u1, u2, u3)) {
+        return distance;
+    }
+
+    // otherwise, return the minimum distance to the edges of face u
+    double min_distance = std::numeric_limits<double>::max();
+    min_distance = std::min(min_distance, point_to_edge_distance(v1, u1, u2));
+    min_distance = std::min(min_distance, point_to_edge_distance(v1, u2, u3));
+    min_distance = std::min(min_distance, point_to_edge_distance(v1, u3, u1));
+    return min_distance;
+}
--- src/mcd-sequential.h
+++ src/mcd-sequential.h
@@ -1,5 +1,82 @@
+
+#include <cmath>
+
 #include "lib/vec3.h"
 #include "lib/obj.h"
+#include "helpers.h"
+
+
+/**
+ * face_to_face_distance - Compute the minimum distance between two faces
+ * 
+ * @param v1 - the first vertex of the first face
+ * @param v2 - the second vertex of the first face
+ * @param v3 - the third vertex of the first face
+ * @param u1 - the first vertex of the second face
+ * @param u2 - the second vertex of the second face
+ * @param u3 - the third vertex of the second face
+ * @return the minimum distance between the two faces
+ */
+static inline double face_to_face_distance(const Vec3& v1, const Vec3& v2, const Vec3& v3,
+                                           const Vec3& u1, const Vec3& u2, const Vec3& u3) {
+    
+#ifdef DEBUG
+    std::cout << "---------- face_to_face_distance ----------" << std::endl;
+    std::cout << "v1: " << v1 << std::endl;
+    std::cout << "v2: " << v2 << std::endl;
+    std::cout << "v3: " << v3 << std::endl;
+    std::cout << "u1: " << u1 << std::endl;
+    std::cout << "u2: " << u2 << std::endl;
+    std::cout << "u3: " << u3 << std::endl;
+    std::cout << "line_face_intersect(v1, v2, u1, u2, u3): " << line_face_intersect(v1, v2, u1, u2, u3) << std::endl;
+    std::cout << "line_face_intersect(v2, v3, u1, u2, u3): " << line_face_intersect(v2, v3, u1, u2, u3) << std::endl;
+    std::cout << "line_face_intersect(v3, v1, u1, u2, u3): " << line_face_intersect(v3, v1, u1, u2, u3) << std::endl;
+#endif
+
+    // first check if the faces intersect
+    if (line_face_intersect(v1, v2, u1, u2, u3) ||
+        line_face_intersect(v2, v3, u1, u2, u3) ||
+        line_face_intersect(v3, v1, u1, u2, u3)) {
+        return 0.0;
+    }
+
+    // then check if the faces are parallel
+    Vec3 normal_v = cross(v2 - v1, v3 - v1).normalize();
+    Vec3 normal_u = cross(u2 - u1, u3 - u1).normalize();
+    if (normal_v == normal_u) {
+        return dot(v1 - u1, normal_v);
+    }
+
+#ifdef DEBUG
+    std::cout << "normal_v: " << normal_v << std::endl;
+    std::cout << "normal_u: " << normal_u << std::endl;
+#endif
+
+    // if not, find the minimum distance 
+    double min_distance = std::numeric_limits<double>::max();
+
+    // find the minimum distance between face u and the vertices of face v
+    min_distance = std::min(min_distance, point_to_face_distance(v1, u1, u2, u3));
+    min_distance = std::min(min_distance, point_to_face_distance(v2, u1, u2, u3));
+    min_distance = std::min(min_distance, point_to_face_distance(v3, u1, u2, u3));
+
+    // find the minimum distance between face u and the edges of face v
+    min_distance = std::min(min_distance, edge_to_face_distance(v1, v2, u1, u2, u3));
+    min_distance = std::min(min_distance, edge_to_face_distance(v2, v3, u1, u2, u3));
+    min_distance = std::min(min_distance, edge_to_face_distance(v3, v1, u1, u2, u3));
+
+#ifdef DEBUG
+    std::cout << "point_to_face_distance(v1, u1, u2, u3): " << point_to_face_distance(v1, u1, u2, u3) << std::endl;
+    std::cout << "point_to_face_distance(v2, u1, u2, u3): " << point_to_face_distance(v2, u1, u2, u3) << std::endl;
+    std::cout << "point_to_face_distance(v3, u1, u2, u3): " << point_to_face_distance(v3, u1, u2, u3) << std::endl;
+    std::cout << "edge_to_face_distance(v1, v2, u1, u2, u3): " << edge_to_face_distance(v1, v2, u1, u2, u3) << std::endl;
+    std::cout << "edge_to_face_distance(v2, v3, u1, u2, u3): " << edge_to_face_distance(v2, v3, u1, u2, u3) << std::endl;
+    std::cout << "edge_to_face_distance(v3, v1, u1, u2, u3): " << edge_to_face_distance(v3, v1, u1, u2, u3) << std::endl;
+#endif
+
+    return min_distance;
+}
+
 
 /**
  * mcd - Mesh Collision Detection
@@ -12,5 +89,47 @@
 */
 double mcd(std::vector< std::vector<Vec3> > model_vertices,
            std::vector< std::vector<Face> > model_faces) {
-    return 0.0;
+
+    double minimum_distance = std::numeric_limits<double>::max();
+    
+    // for each model[i] (defined by a vector of vertices and a vector of faces)...
+    for (size_t i = 0; i < model_faces.size(); i++) {
+        // loop through all other models in the scene...
+        for (size_t j = 0; j < model_faces.size(); j++) {
+            if (i == j) {
+                continue;
+            }
+            // if the models are not the same, for all the faces in model[i]...
+            for (size_t k = 0; k < model_faces[i].size(); k++) {
+                // loop through all the faces in model[j]...
+                for (size_t l = 0; l < model_faces[j].size(); l++) {
+                    // find the minimum distance between the two faces
+                    double distance = face_to_face_distance(
+                        model_vertices[i][model_faces[i][k].v1],
+                        model_vertices[i][model_faces[i][k].v2],
+                        model_vertices[i][model_faces[i][k].v3],
+                        model_vertices[j][model_faces[j][l].v1],
+                        model_vertices[j][model_faces[j][l].v2],
+                        model_vertices[j][model_faces[j][l].v3]
+                    );
+                    if (distance < minimum_distance) {
+                        minimum_distance = distance;
+                    }
+
+#ifdef DEBUG
+                    std::cout << "---------- mcd ----------" << std::endl;
+                    std::cout << "i: " << i << std::endl;
+                    std::cout << "j: " << j << std::endl;
+                    std::cout << "k: " << k << std::endl;
+                    std::cout << "l: " << l << std::endl;
+                    std::cout << "distance: " << distance << std::endl;
+                    std::cout << "minimum_distance: " << minimum_distance << std::endl;
+#endif
+
+                }
+            }
+        }
+    }
+
+    return minimum_distance;
 }
